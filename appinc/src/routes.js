088805e7a38df7e664eccf3a78a3a7b4ca9c80dc
ayrtonlacerda <ayrtonrodrigues.lacerda@gmail.com>
2018-12-10 11:49:17 -0200

    feat: validation

--- appinc/src/routes.js
+++ appinc/src/routes.js
@@ -22,7 +22,7 @@ import StepList from './pages/StepList';
 
 const Routes = StackNavigator(
   {
-    Login: { screen: StepList },
+    Login: { screen: New },
     StepPage: { screen: StepPage },
     Logged: createDrawerNavigator(
       {
