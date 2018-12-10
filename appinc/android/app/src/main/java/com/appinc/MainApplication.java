1f627ec22f236267c5ddcc1b427c4b8b6f0b4cd8
Unknown <ayrtonlaceda01@gmail.com>
2018-12-10 21:12:17 -0200

    bug: image picker

--- appinc/android/app/src/main/java/com/appinc/MainApplication.java
+++ appinc/android/app/src/main/java/com/appinc/MainApplication.java
@@ -3,7 +3,6 @@ package com.appinc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.imagepicker.ImagePickerPackage;
 import com.airbnb.android.react.lottie.LottiePackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.zmxv.RNSound.RNSoundPackage;
@@ -29,8 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ImagePickerPackage(),
+          new MainReactPackage(),            
             new LottiePackage(),
             new ReactNativeAudioPackage(),
             new RNSoundPackage(),
