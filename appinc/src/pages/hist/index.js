580dbc324a04d8005e6426cc281a7e3568d561b1
Ayrton Lacerda <ayrtonlaceda01@gmail.com>
2018-12-14 12:49:42 -0200

    feat: menu

---
+++ appinc/src/pages/hist/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class History extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
