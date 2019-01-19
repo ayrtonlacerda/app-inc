a5607a16a374a97e2d927169a8ae19ff93260831
Paulo Henrique Ferreira <paulohfcmendes@gmail.com>
2019-01-18 23:05:24 -0200

    fix: componente câmera

--- appinc/src/components/InputText/index.js
+++ appinc/src/components/InputText/index.js
@@ -12,7 +12,7 @@ import styles from './styles';
 
 class InputText extends Component {
   state = {
-    inputSave: null,
+    inputSave:'',
   }
 
   componentDidMount() {
--- appinc/src/components/camera/index.js
+++ appinc/src/components/camera/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import { Text, View, TouchableOpacity, Image, TextInput } from 'react-native';
-// import ImagePicker from 'react-native-image-picker';
+import React, {Component} from 'react';
+import {
+  View, Text, StyleSheet, ScrollView, Alert,
+  Image, TouchableOpacity, NativeModules, Dimensions, TextInput, AsyncStorage
+} from 'react-native';
+
+import Video from 'react-native-video';
 import styles from './styles';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+var ImagePicker = NativeModules.ImageCropPicker;
+
 
 class Camera extends React.Component {
 
@@ -12,63 +19,164 @@ class Camera extends React.Component {
     videoSource: null
   };
 
-  selectPhotoTapped() {
-    const options = {
-      quality: 1.0,
-      maxWidth: 500,
-      maxHeight: 500,
-      storageOptions: {
-        skipBackup: true
-      }
+  constructor() {
+    super();
+    this.state = {
+      image: null,
+      images: null
     };
+  }
 
-    ImagePicker.showImagePicker(options, (response) => {
-     // console.tron.log('Response = ', response);
+  pickSingleWithCamera(cropping) {
+    ImagePicker.openCamera({
+      cropping: cropping,
+      width: 500,
+      height: 500,
+      includeExif: false,
+      includeBase64: true,
+    }).then(image => {
+   
+      this.setState({
+        image: {uri: image.path, width: image.width, height: image.height,},
+        images: null
+      });
+      console.tron.log('received image', image.data);
+     
+    }).catch(e => alert(e));
+  }
 
-      if (response.didCancel) {
-        console.log('User cancelled photo picker');
-        }
-      else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      }
-      else {
-        let source = { response };
-        this.setState({
-          avatarSource: source
-        });
-      }
+
+
+  pickSingleBase64(cropit) {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: cropit,
+      includeBase64: true,
+      includeExif: true,
+    }).then(image => {
+      console.log('received base64 image');
+      this.setState({
+        image: {uri: `data:${image.mime};base64,`+ image.data, width: image.width, height: image.height},
+        images: null
+      });
+    }).catch(e => alert(e));
+  }
+
+  cleanupImages() {
+    ImagePicker.clean().then(() => {
+      console.log('removed tmp images from tmp directory');
+    }).catch(e => {
+      alert(e);
     });
   }
 
-  selectVideoTapped() {
-    const options = {
-      title: 'Gravar vídeo',
-      takePhotoButtonTitle: 'Gravar',
-      mediaType: 'video',
-      videoQuality: 'high'
-    };
+  cleanupSingleImage() {
+    let image = this.state.image || (this.state.images && this.state.images.length ? this.state.images[0] : null);
+    console.log('will cleanup image', image);
 
-    ImagePicker.showImagePicker(options, (response) => {
-      console.log('Response = ', response);
+    ImagePicker.cleanSingle(image ? image.uri : null).then(() => {
+      console.log(`removed tmp image ${image.uri} from tmp directory`);
+    }).catch(e => {
+      alert(e);
+    })
+  }
 
-      if (response.didCancel) {
-        console.log('User cancelled video picker');
-      }
-      else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      }
-      else {
-        this.setState({
-          videoSource: response.uri
-        });
-      }
+  cropLast() {
+    if (!this.state.image) {
+      return Alert.alert('No image', 'Before open cropping only, please select image');
+    }
+
+    ImagePicker.openCropper({
+      path: this.state.image.uri,
+      width: 200,
+      height: 200
+    }).then(image => {
+      console.log('received cropped image', image);
+      this.setState({
+        image: {uri: image.path, width: image.width, height: image.height, mime: image.mime},
+        images: null
+      });
+    }).catch(e => {
+      console.log(e);
+      Alert.alert(e.message ? e.message : e);
     });
+  }
+
+  pickSingle(cropit, circular=false) {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: cropit,
+      cropperCircleOverlay: circular,
+      compressImageMaxWidth: 640,
+      compressImageMaxHeight: 480,
+      compressImageQuality: 0.5,
+      compressVideoPreset: 'MediumQuality',
+      includeExif: true,
+    }).then(image => {
+      console.log('received image', image);
+      this.setState({
+        image: {uri: image.path, width: image.width, height: image.height, mime: image.mime},
+        images: null
+      });
+    }).catch(e => {
+      console.log(e);
+      Alert.alert(e.message ? e.message : e);
+    });
+  }
+
+  pickMultiple() {
+    ImagePicker.openPicker({
+      multiple: true,
+      waitAnimationEnd: false,
+      includeExif: true,
+      forceJpg: true,
+    }).then(images => {
+      this.setState({
+        image: null,
+        images: images.map(i => {
+          console.log('received image', i);
+          return {uri: i.path, width: i.width, height: i.height, mime: i.mime};
+        })
+      });
+    }).catch(e => alert(e));
+  }
+
+  scaledHeight(oldW, oldH, newW) {
+    return (oldH / oldW) * newW;
+  }
+
+  renderVideo(video) {
+    return (<View style={{height: 300, width: 300}}>
+      <Video source={{uri: video.uri, type: video.mime}}
+         style={{position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0
+          }}
+         rate={1}
+         paused={false}
+         volume={1}
+         muted={false}
+         resizeMode={'cover'}
+         onError={e => console.log(e)}
+         onLoad={load => console.log(load)}
+         repeat={true} />
+     </View>);
+  }
+
+  renderImage(image) {
+    return <Image style={{width: 300, height: 300, resizeMode: 'contain'}} source={image} />
+  }
+
+  renderAsset(image) {
+    if (image.mime && image.mime.toLowerCase().indexOf('video/') !== -1) {
+      return this.renderVideo(image);
+    }
+
+    return this.renderImage(image);
   }
 
   render() {
@@ -76,7 +184,7 @@ class Camera extends React.Component {
     return (
       <View style={styles.container}>
 
-        <TouchableOpacity onPress={this.selectPhotoTapped.bind(this)}>
+        <TouchableOpacity onPress={() => this.pickSingleWithCamera(false)}>
           <View style = {styles.avatarContainer}>
           { this.state.avatarSource === null ? <View style = {styles.avatarContainer2}><Icon name="add-a-photo" size={30} style={styles.icon} />
           <View style = {styles.text_foto}><Text>Tirar uma foto</Text></View></View>:
